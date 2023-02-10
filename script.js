789f2f45ac0694e2854423af621487ea696084f4
Mari Azh <Marzi.aghchay@gmail.com>
2023-02-10 18:47:48 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const checkDog = function(myDoges , yourDoge){
     myCorrectDog.splice(0,1);
     myCorrectDog.splice(-2)
     console.log(myCorrectDog);
+    const dogs =myCorrectDog.concat(yourDoge);
+   
+    dogs.forEach(function(dog,i){
+       if(dog >= 3){
+        console.log(`dog number${i+1} is an adult,${dog} years old `);
+       }else{
+        console.log(`dog number${i+1} is an puppy,${dog} years old `);
+       }
+    })
 
 }
 checkDog([9,10,3,2,11],[3,18,5,7,1])
